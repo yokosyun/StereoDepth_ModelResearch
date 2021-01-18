c1ff32ce1af3a6724ab5b0d7067fc81020034b8e
yokoko <yoko.syun.1408@gmail.com>
2021-01-19 08:56:16 +0900

    calc ave time

--- inference.sh
+++ inference.sh
@@ -1,6 +1,5 @@
 python inference.py --KITTI 2015 \
 --datapath /media/yoko/SSD-PGU3/workspace/datasets/KITTI/data_scene_flow/training/ \
---loadmodel result/concatNet_disp192_sizeFull.tar \
+--loadmodel result/weights/concatNet299.tar \
 --model concatNet \
---maxdisp 192 \
---seed 1
+--maxdisp 192
