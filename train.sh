f7359ea60725d143b8ef048e45b082aba90c3e31
yokoko <yoko.syun.1408@gmail.com>
2021-01-27 18:13:51 +0900

    add CorrSMNet_Sigmoid

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 python train.py --maxdisp 192 \
-               --model FCSMNet \
+               --model CorrSMNet_Sigmoid \
                --datapath /media/yoko/SSD-PGU3/workspace/datasets/KITTI/data_scene_flow/training/ \
                --epochs 300 \
                --savemodel ./result
